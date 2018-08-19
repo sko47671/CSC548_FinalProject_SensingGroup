15f1b0d8de841e2456e889271d27013ba91de69f
balboa342 <42502796+balboa342@users.noreply.github.com>
2018-08-18 19:24:40 -0500

    Add HelloClass.java to the Repository

---
+++ HelloClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloclass;
+
+/**
+ *
+ * @author bkoken
+ */
+
+public class HelloClass {
+  HelloClass() {
+    System.out.println("This application introduces the class.");
+  }
+ 
+  public static void main(String[] args) {
+    HelloClass my = new HelloClass();
+    BKokensparger BJK = new BKokensparger();
+ 
+  }
+}
+ 
+class BKokensparger {
+  BKokensparger() {
+    System.out.println("Hi, I'm Brian Kokensparger, originally from Junction City, Ohio. "
+            + "I am your instructor for this course.");
+  }
+} 
