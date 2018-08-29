bdbf8c2ce095db4ec94e7e4b6a252e633c6070c6
David-Y-Lee <dyl38028@creighton.edu>
2018-08-29 14:30:49 -0500

    DLee added
    DLee added

--- HelloClass.java
+++ HelloClass.java
@@ -9,22 +9,32 @@ package helloclass;
  *
  * @author bkoken
  */
-
 public class HelloClass {
-  HelloClass() {
-    System.out.println("This application introduces the class.");
-  }
- 
-  public static void main(String[] args) {
-    HelloClass my = new HelloClass();
-    BKokensparger BJK = new BKokensparger();
- 
-  }
+
+    HelloClass() {
+        System.out.println("This application introduces the class.");
+    }
+
+    public static void main(String[] args) {
+        HelloClass my = new HelloClass();
+        BKokensparger BJK = new BKokensparger();
+
+    }
 }
- 
+
 class BKokensparger {
-  BKokensparger() {
-    System.out.println("Hi, I'm Brian Kokensparger, originally from Junction City, Ohio. "
-            + "I am your instructor for this course.");
-  }
-} 
+
+    BKokensparger() {
+        System.out.println("Hi, I'm Brian Kokensparger, originally from Junction City, Ohio. "
+                + "I am your instructor for this course.");
+    }
+}
+
+class DLee {
+
+    DLee() {
+        System.out.println("Hi, I'm David Lee, originally from South Korea. "
+                + "I studied in India for four years in middle school");
+    }
+}
+
