fe04fb8feac3030eb3f59a08a89a0de3c9526fdb
LDubbsBell <42784102+LDubbsBell@users.noreply.github.com>
2018-08-29 14:44:07 -0500

    Update HelloClass.java

--- HelloClass.java
+++ HelloClass.java
@@ -18,11 +18,9 @@ public class HelloClass {
   public static void main(String[] args) {
     HelloClass my = new HelloClass();
     BKokensparger BJK = new BKokensparger();
-
-    LBell LWB = new LBell();
-
+    RLabausa RTL = new RLabausa();
     DTheis DJT = new DTheis();
- 
+    PJohnson PLJ = new PJohnson();
   }
 }
  
@@ -33,16 +31,41 @@ class BKokensparger {
   }
 } 
 
-class LBell {
-  LBell() {
-    System.out.println("Hi, I'm Logan Bell, originally from Grand Island, Nebraska. " 
-                       + "I'm not your instructor for this course.")
+
+class CYu {
+  CYu() {
+    System.out.println("Hi, I'm Catherine Yu, originally from Seoul, Korea. "
+            + "I am a senior .");
   }
 }
+
+class LBell {
+  LBell() {
+    System.out.println("Hi, I'm Logan Bell. I am from Grand Island, Nebraska."
+            + "I am a student in this course.");
+  }
+} 
+
+class RLabausa {
+  RLabausa() {
+    System.out.println("Hi, I'm Ruby Labausa. I am from Ewa Beach, Hawaii."
+            + "I am a student in this course.");
+  }
+} 
+
 class DTheis {
   DTheis() {
     System.out.println("Hi, I'm Dominic Theis and I am from Stillwater, MN. "
             + "I am a senior graduating in may.");
   }
 } 
+
+
+class PJohnson {
+  PJohnson() {
+    System.out.println("Hi, I'm Parker Johnson, originally from Burnsville, MN. "
+            + "I am NOT your instructor for this course.");
+  }
+} 
   
+
